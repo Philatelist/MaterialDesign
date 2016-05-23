b8a301e1ca0fd15379da16d7f0d355feec2b77de
Alexander Usov <a.usoff@gmail.com>
2016-05-23 22:25:14 +0300

    Finished Lesson2 - about Floating Action Button

--- less3_floatingactionbutton/src/main/java/com/slavyanin/example/less3_floatingactionbutton/MainActivity.java
+++ less3_floatingactionbutton/src/main/java/com/slavyanin/example/less3_floatingactionbutton/MainActivity.java
@@ -2,6 +2,10 @@ package com.slavyanin.example.less3_floatingactionbutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.melnykov.fab.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.attachToListView(listView);
+
+        String[] items = new String[20];
+        for (int i = 0; i < 20; i++) {
+            items[i] = "item [ " + (i + 1) + "]";
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
     }
 }

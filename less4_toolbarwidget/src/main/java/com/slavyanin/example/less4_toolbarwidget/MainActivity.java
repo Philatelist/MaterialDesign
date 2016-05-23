4493e7706c8f0251130e849ac85373e39792279d
Alexander Usov <a.usoff@gmail.com>
2016-05-23 16:33:58 +0300

    Add Lesson4 - about Toolbar Widget.

---
+++ less4_toolbarwidget/src/main/java/com/slavyanin/example/less4_toolbarwidget/MainActivity.java
@@ -0,0 +1,20 @@
+package com.slavyanin.example.less4_toolbarwidget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}

772cde63fb586f648bb9d1b79e225d5af0ad4049
Alexander Usov <a.usoff@gmail.com>
2016-05-25 02:12:21 +0300

    Add Lesson7 - about Snackbar, CoordinatorLayout, FloatingActionButton

---
+++ less7_snackbar/src/main/java/com/slavyanin/example/less7_snackbar/MainActivity.java
@@ -0,0 +1,24 @@
+package com.slavyanin.example.less7_snackbar;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final View content = findViewById(R.id.coordinator);
+
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(content, "My first Snackbar", Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+}
